1d68b167aaf028c8f2c1d2a11d6fb3353dacf580
Daniel Valencia Backhoff <danvalencia@gmail.com>
2016-10-27 11:26:21 -0500

    Initial commit from the atomist bot

---
+++ src/main/java/com/danvalencia/TodoServiceConfiguration.java
@@ -0,0 +1,9 @@
+package com.danvalencia;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TodoServiceConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
